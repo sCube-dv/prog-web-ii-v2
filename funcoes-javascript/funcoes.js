fbb0ae537e9b569c055a149420e0b886c95552c5
sCube-dv <santos.samuel1@acad.ifma.edu.br>
2025-09-23 16:21:28 -0300

    resposta ex5

--- funcoes-javascript/funcoes.js
+++ funcoes-javascript/funcoes.js
@@ -7,7 +7,7 @@ const calculaPorcetagem = (valor, porcentagem) => {
     return valorFinal
 }
 /* teste da função */
-console.log(`1. ${calculaPorcetagem(100, 10)}`) // 110
+// console.log(`1. ${calculaPorcetagem(100, 10)}`) // 110
 
 /* 
 2. Desenvolva uma função que determine se um número é par ou não. Retorne true se for par e 
@@ -20,7 +20,7 @@ const ehPar = (n) => {
     return par
 }
 /* teste da função */
-console.log(`2. ${ehPar(7)}`)
+// console.log(`2. ${ehPar(7)}`)
 
 /* 
 3. Implemente uma função que inverta a ordem dos elementos em um array. Utilize essa função 
@@ -32,7 +32,7 @@ const inverteArray = (array) => {
     return arrayInvertido
 }
 /* teste da função */
-console.log(`3. ${inverteArray([1, 2, 3, 4, 5])}`)
+// console.log(`3. ${inverteArray([1, 2, 3, 4, 5])}`)
 
 /* 
 4. Crie uma função que receba um vetor, um valor v e retorne a posição de v no vetor
@@ -46,4 +46,26 @@ const retornaPosVet = (vetor, valor) => {
 }
 
 /* teste da função */
-console.log(`4. ${retornaPosVet([1, 2, 3, 4, 5], 0)}`)
+// console.log(`4. ${retornaPosVet([1, 2, 3, 4, 5], 5)}`)
+
+/* 
+5. Escreva uma função de converter vetor em objeto. A função deve receber 3 valores, ID, Nome, Telefone e E-mail, e retorne um objeto com esses valores
+*/
+
+let vetorParaObj = (id, nome, tel, email) => {
+   
+    let pessoa = [
+        ['ID', id],
+        ['nome', nome],
+        ['telefone', tel],
+        ['email', email]
+    ]
+
+    let pessoaObj = Object.fromEntries(pessoa)
+
+    return pessoaObj
+
+}
+
+//console.log(`5. ${vetorParaObj(1, 'Samuel', 2367854329, 'sam@emal.com')}`)
+console.log(vetorParaObj(1, 'Samuel', 99887766, 'sam@emal.com'))
